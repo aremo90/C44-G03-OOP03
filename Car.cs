c0ea9ecded92df8609a7ef85afd3d5ec1ed6c3e2
aremo90 <mrhamza2005@gmail.com>
2025-07-28 03:51:41 +0300

    02 Class

---
+++ Car.cs
@@ -0,0 +1,59 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Demo
+//{
+//    class Car
+//    {
+//        #region attributes
+
+//        //private int id;
+//        //private string? model;
+//        //private double speed;
+
+//        #endregion
+
+//        #region properties
+//        // automatic properties
+
+//        public int id { get; set; }
+//        public string? model { get; set; }
+//        public double speed { get; set; }
+
+
+//        #endregion
+
+//        #region constructors
+//        public Car(int Id, string? Model, double Speed)
+//        {
+//            id = Id;
+//            model = Model;
+//            speed = Speed;
+//            Console.WriteLine("1st CTOR");
+//        }
+//        // CTOR overloading
+//        public Car(int Id, string? Model):this(Id , Model , 350)
+//        {
+//            //id = Id;
+//            //model = Model;
+//            Console.WriteLine("2nd CTOR");
+
+//        }
+//        // CTOR Chaining
+//        public Car(int Id) : this(Id, "Audi" ,200)
+//        {
+//            Console.WriteLine("3rd CTOR");
+//        }
+//        #endregion
+
+//        #region methods
+//        public override string ToString()
+//        {
+//                return $"Car Id: {id}, Model: {model}, Speed: {speed} km/h";
+//        }
+//        #endregion
+//    }
+//}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region User defined Data Types [Classes]
+            /*
+             * Blueprint => contain objects
+             * Reference Type objects of class stored in stack and values stored in heap
+             * compiler will generate empty parameterless constructor
+             * if a user defined constructor found , compiler will no longer need parameterless constructor
+             *  - Attributes
+             *  - Functions
+             *  - Properties
+             *  - events
+             * 
+             * 
+             *  - 6 Access Modifiers
+             *  - default Access Modifier for class members is private
+             *  - Default Access Modifier for classes is internal
+             */
+
+            //Car c1;
+            // Declare for reference type from 'Car' class
+            // c1 refer to defult value in heap
+            // This reference c1 can refer to an instance of the class Car or any data type that inherits from Car.
+            // CLR will allocate 4 byte at stacj for object 'C1'
+
+            //c1 = new Car( 10 , "BMW" , 300 );
+            //Console.WriteLine(c1);
+
+            //Car c2 = new Car( 20 , "Mercedes" );
+            //Console.WriteLine(c2);
+
+            //Car c3 = new Car( 30 );
+            //Console.WriteLine(c3);
+
+            #endregion
         }
     }
 }
